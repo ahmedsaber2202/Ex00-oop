613c46bbf398e5968b74a2a7829424df7f96869f
ahmedsaber2202 <ahmed.ahmed@stud.th-rosenheim.de>
2026-04-23 15:32:07 +0200

    update 01

---
+++ src/main/java/CCC.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CCC {
+    public static String readInput ()
+    {
+        Scanner sc = new Scanner(System.in);
+        String c = sc.nextLine();
+        return c;
+    }
+    public static void main(String[] args) {
+        System.out.println(readInput());
+    }
+}
